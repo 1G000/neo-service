d0f834b0dbd7e594fcf93f602d10ebddc30ca367
1G000 <gorbatovskiy.igor@gmail.com>
2024-10-23 21:57:45 +0300

    feat: add logo

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <img src="/src/components/icons/Group 12 (1) 1.png" class="logo" alt="" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  width: 70px;
+  height: 80px;
+  background-color: var(--vt-c-header-bg);
+}
+</style>
--- src/components/TheWelcome.vue
+++
@@ -1,5 +0,0 @@
-<script setup></script>
-
-<template>
-  <h1>ZDAROVO</h1>
-</template>
